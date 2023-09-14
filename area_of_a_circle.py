e86320f92da0a370d70e893c134963b035b798c9
Jen Kim <jen.bokyung.kim@gmail.com>
2023-09-14 16:31:46 -0500

    Changed the radius

--- area_of_a_circle.py
+++ area_of_a_circle.py
@@ -1,9 +1,11 @@
+# This calculates the area of a circle with a given radius
+
 def compute_area_of_circle(radius):
 	pi = 3.14
 	area = pi * radius * radius
 	return area
 
-radius1 = 30
+radius1 = 20
 area1 = compute_area_of_circle(radius1)
 print(f"The area of circle with radius {radius1} is: {area1}")
 
