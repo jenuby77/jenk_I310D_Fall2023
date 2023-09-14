e14e3b47876c6994eb9ae24acf45c5a33f9f2cda
Jen Kim <jen.bokyung.kim@gmail.com>
2023-09-14 16:53:17 -0500

    added new file

---
+++ volume_of_sphere.py
@@ -0,0 +1,18 @@
+import math
+
+def calculate_volume_of_sphere (radius):
+    volume = (4/3) * math.pi * radius**3
+    return volume 
+
+radius1 = 30
+area1 = calculate_volume_of_sphere(radius1)
+print (f"The area of a circle with radius {radius1} is: {area1}")
+       
+radius2 = 40
+area2 = calculate_volume_of_sphere(radius2)
+print (f"The area of a circle with radius {radius2} is: {area2}")
+
+
+
+
+
